0af2d7ff83fd11b5838cc4779139fe4946614733
Rafael Cabral <rafaelsantanacabral@gmail.coma>
2020-09-08 20:29:09 -0300

    feat: fetch hackers clone api for stories

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -1,5 +1,20 @@
 import view from '../utils/view.js';
 
-export default function Stories(path) {
+export default async function Stories(path) {
+  const stories = await getStories(path);
+  console.log(stories);
   view.innerHTML = `<div>${path}</div>`;
+}
+
+async function getStories(path) {
+  const isHomeRoute = path === '/';
+
+  if (isHomeRoute) {
+    path = '/news';
+  }
+
+  const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
+  const stories = await response.json();
+
+  return stories;
 }
