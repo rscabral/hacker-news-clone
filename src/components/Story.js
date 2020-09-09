638a5ea32ba0e9bc6352d449485b1c0b111d37a9
Rafael Cabral <rafaelsantanacabral@gmail.coma>
2020-09-08 21:07:12 -0300

    feat: add newest stories

---
+++ src/components/Story.js
@@ -0,0 +1,26 @@
+export default function Story(story) {
+  // console.log(story);
+  return `
+  <div class="story">
+    <div>
+      <span class="gray">${story.index}</span>
+      <span class="upvote">▲</span>
+      <a href="${story.url}">${story.title}</a>
+      <a href="${story.domain}">(${story.domain})</a>      
+    </div>
+    <div>
+      <div class="gray">
+        ${story.points} points by ${story.user} ${story.time_ago}
+        |
+        <a href="#/item?id=${story.id}">
+          ${story.comments_count}
+        </a>
+        <span class="favorite">
+          <img class="heart" src="../../assets/favorite-icon.png">
+          Add To Favorites
+        </span>
+      </div>
+    </div>
+  </div>
+  `;
+}
--- src/pages/stories.js
+++ src/pages/stories.js
@@ -1,16 +1,24 @@
 import view from '../utils/view.js';
+import Story from '../components/Story.js';
 
 export default async function Stories(path) {
-  const stories = await getStories(path);
-  console.log(stories);
-  view.innerHTML = `<div>${path}</div>`;
+  const stories = await getStories(path);  
+  const hasStories = stories.length > 0;
+  view.innerHTML =
+    `<div>
+      ${hasStories ? stories.map((story, index) => Story({...story, index: index + 1})).join('') 
+        : 'No stories found;'}      
+    </div>`;
 }
 
 async function getStories(path) {
   const isHomeRoute = path === '/';
+  const isNewRoute = path === '/new';
 
   if (isHomeRoute) {
     path = '/news';
+  } else if (isNewRoute) {
+    path = '/newest';
   }
 
   const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
