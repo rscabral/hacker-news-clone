b44e54f4ba640f4f0347072beacac04f610e0752
Rafael Cabral <rafaelsantanacabral@gmail.coma>
2020-11-07 16:21:33 -0300

    feat: add ask and show api paths

--- src/api/pathMap.js
+++ src/api/pathMap.js
@@ -1,8 +1,12 @@
 import homeRoute from './homeRoute.js';
 import newRoute from './newRoute.js';
+import askRoute from './askRoute.js';
+import showRoute from './showRoute.js';
 
 const pathMap = new Map();
 pathMap.set('/', homeRoute);
 pathMap.set('/new', newRoute);
+pathMap.set('/ask', askRoute);
+pathMap.set('/show', showRoute);
 
 export default pathMap;
