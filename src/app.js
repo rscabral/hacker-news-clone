34206a9b8ee6a81a35a41a40441ca6b111b39f48
Rafael Cabral <rafaelsantanacabral@gmail.coma>
2020-11-07 17:54:18 -0300

    feat: add visual indicator of current page at nav bar

--- src/app.js
+++ src/app.js
@@ -1,4 +1,26 @@
 import RouterHandler from './router.js';
+
+/* This callback available on window variable listen to any change on hash of
+  url. So, anytime I navigate through our menu, this callback will be called
+*/ 
+window.onhashchange = () => {
+  setActiveLink();
+}
+
+function setActiveLink() {
+  const links = document.querySelectorAll('.header-link');
+  links.forEach(link => {
+    const linkPath = link.getAttribute('href');
+    const currentPath = window.location.hash;
+
+    if (currentPath === linkPath) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+}
+
 class App {
   constructor() {
     new RouterHandler();
